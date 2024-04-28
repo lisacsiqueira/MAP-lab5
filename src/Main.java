8b69234a31827645ce1b95d082ab16250b5173de
lisacsiqueira <lisacosta.as@gmail.com>
2024-04-28 19:21:16 -0300

    Adicionado teste para a classe SistemaFacade

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import entities.*;
-import java.util.Map;
+import facade.SistemaFacade;
 
 public class Main {
     public static void main(String[] args) {
-        // Criando instâncias dos subsistemas
-        Administrativo administrativo = new Administrativo();
-        Financeiro financeiro = new Financeiro();
-        Almoxarifado almoxarifado = new Almoxarifado();
+        SistemaFacade facade = new SistemaFacade();
 
-        // Testando o subsistema administrativo
-        administrativo.agendarReuniao("Reunião de diretoria");
-        administrativo.agendarReuniao("Reunião de equipe");
-        administrativo.agendarEntrevista("Entrevista com candidato 1");
-        administrativo.agendarEntrevista("Entrevista com candidato 2");
+        // Agendar reuniões e entrevistas
+        facade.agendarReuniao("Reunião de diretoria");
+        facade.agendarReuniao("Reunião de equipe");
+        facade.agendarEntrevista("Entrevista com candidato 1");
+        facade.agendarEntrevista("Entrevista com candidato 2");
 
+        // Atualizar balanço de contas e processar folha de pagamento
+        facade.atualizarBalancoContas("Balanço mensal", 50000.0);
+        facade.atualizarBalancoContas("Reservas", 20000.0);
+        facade.processarFolhaPagamento("Funcionário 1", 3000.0);
+        facade.processarFolhaPagamento("Funcionário 2", 2500.0);
+
+        // Atualizar estoque e registrar pedido de compra
+        facade.atualizarEstoque("Canetas", 100);
+        facade.atualizarEstoque("Papéis", 200);
+        facade.registrarPedidoCompra("Toner", 5);
+
+        // Obter informações do sistema
         System.out.println("Reuniões agendadas:");
-        for (String reuniao : administrativo.getReunioesAgendadas()) {
-            System.out.println("- " + reuniao);
-        }
+        facade.getReunioesAgendadas().forEach(System.out::println);
 
         System.out.println("\nEntrevistas agendadas:");
-        for (String entrevista : administrativo.getEntrevistas()) {
-            System.out.println("- " + entrevista);
-        }
-
-        // Testando o subsistema financeiro
-        financeiro.atualizarBalancoContas("Balanço mensal", 50000.0);
-        financeiro.atualizarBalancoContas("Reservas", 20000.0);
-        financeiro.processarFolhaPagamento("Funcionário 1", 3000.0);
-        financeiro.processarFolhaPagamento("Funcionário 2", 2500.0);
+        facade.getEntrevistas().forEach(System.out::println);
 
         System.out.println("\nBalanço de contas:");
-        for (Map.Entry<String, Double> entry : financeiro.getBalancoContas().entrySet()) {
-            System.out.println("- " + entry.getKey() + ": $" + entry.getValue());
-        }
+        facade.getBalancoContas().forEach((descricao, valor) ->
+                System.out.println("- " + descricao + ": $" + valor));
 
         System.out.println("\nFolha de pagamento:");
-        for (Map.Entry<String, Double> entry : financeiro.getFolhaPagamento().entrySet()) {
-            System.out.println("- " + entry.getKey() + ": $" + entry.getValue());
-        }
-
-        // Testando o subsistema de almoxarifado
-        almoxarifado.atualizarEstoque("Canetas", 100);
-        almoxarifado.atualizarEstoque("Papéis", 200);
-        almoxarifado.registrarPedidoCompra("Toner", 5);
+        facade.getFolhaPagamento().forEach((funcionario, salario) ->
+                System.out.println("- " + funcionario + ": $" + salario));
 
         System.out.println("\nEstoque atual:");
-        for (Map.Entry<String, Integer> entry : almoxarifado.getEstoque().entrySet()) {
-            System.out.println("- " + entry.getKey() + ": " + entry.getValue() + " unidades");
-        }
+        facade.getEstoque().forEach((item, quantidade) ->
+                System.out.println("- " + item + ": " + quantidade + " unidades"));
 
         System.out.println("\nPedidos de compra:");
-        for (Map.Entry<String, Integer> entry : almoxarifado.getPedidosCompra().entrySet()) {
-            System.out.println("- " + entry.getKey() + ": " + entry.getValue() + " unidades");
-        }
+        facade.getPedidosCompra().forEach((item, quantidade) ->
+                System.out.println("- " + item + ": " + quantidade + " unidades"));
     }
 }
